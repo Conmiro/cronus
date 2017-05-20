e66186bca2653f1953d0313b02dac080c2690f22
Connor Romeros <Conmiro@live.com>
2017-05-19 20:41:45 -0700

    added comments and removed unnecessary files

--- webchat/consumers.py
+++ webchat/consumers.py
@@ -6,43 +6,13 @@ class ChatServer(JsonWebsocketConsumer):
     # Set to True if you want it, else leave it out
     strict_ordering = False
 
-    def connection_groups(self, **kwargs):
-        """
-        Called to return the list of groups to automatically add/remove
-        this connection to/from.
-        """
-        return ["test"]
-
-
-    # don't really need this here for current usage.
+    # called when the socket is created
     def connect(self, message, **kwargs):
-        """
-        Perform things on connection start
-        """
-        # Accept the connection; this is done by default if you don't override
-        # the connect function.
         data = {'username': "Server", 'message': "Someone has joined!"}
         self.send(data)
         self.message.reply_channel.send({"accept": True})
 
+    # called when a message is sent from client
     def receive(self, content, **kwargs):
-        """
-        Called when a message is received with decoded JSON content
-        """
-        print(content)
+        # send to all clients in this group (aka ChatServer)
         self.send(content)
-
-    def disconnect(self, message, **kwargs):
-        """
-        Perform things on connection close
-        """
-        pass
-
-    # Optionally provide your own custom json encoder and decoder
-    # @classmethod
-    # def decode_json(cls, text):
-    #     return my_custom_json_decoder(text)
-    #
-    # @classmethod
-    # def encode_json(cls, content):
-    #     return my_custom_json_encoder(content)
--- webchat/views.py
+++ webchat/views.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render
-from channels import Group
-# Create your views here.
-from webchat.models import *
 
 
 def index(request):
 
-    if request.method == 'POST':
-        owner = request.POST.get('username')
-        message_text = request.POST.get('message')
-        #message = Message(owner=owner, message = message_text)
-        #message.save()
-
     return render(request, "webchat/index.html")
-
-
-def conversation(request):
-
-    #convo = Message.objects.all()
-
-    #context = {'convo': convo}
-
-    # return render(request, "webchat/conversation.html", context)
-    return render(request, "webchat/conversation.html")
-
