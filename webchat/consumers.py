55c6b65261261ba1fd1b275e8bc4482d32a8be08
Connor Romeros <conmiro@live.com>
2017-06-03 01:19:32 -0500

    some cleanup and commenting

--- webchat/consumers.py
+++ webchat/consumers.py
@@ -3,8 +3,6 @@ import json
 from channels.generic.websockets import JsonWebsocketConsumer
 from channels import Group
 
-
-
 class ChatServer(JsonWebsocketConsumer):
 
     # Set to True if you want it, else leave it out
@@ -13,7 +11,6 @@ class ChatServer(JsonWebsocketConsumer):
     # called when the socket is created
     def connect(self, message, **kwargs):
         data = {'username': "Server", 'message': "Someone has joined!"}
-        # self.send(data)
         self.message.reply_channel.send({"accept": True})
         Group("chat").add(self.message.reply_channel)
         Group("chat").send({'text': json.dumps(data)})
@@ -21,14 +18,7 @@ class ChatServer(JsonWebsocketConsumer):
     # called when a message is sent from client
     def receive(self, content, **kwargs):
         # send to all clients in this group (aka ChatServer)
-        print("test")
-        # self.send(content)
         Group("chat").send({'text': json.dumps(content)})
 
-    def connection_groups(self, **kwargs):
-        """
-        Called to return the list of groups to automatically add/remove
-        this connection to/from.
-        """
-        return ["test"]
+
 
